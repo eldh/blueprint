22682a04548cd2691d4350a3420bafa993de77f2
eldh <andreas.eldh@gmail.com>
2012-12-15 16:27:51 +0100

    Updated inuit and added some things.
    This whole thing is still very much wip and not very organized...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+	$(".fullscreen-box").css("height", window.innerHeight);
+	$(".placekitten").each(function(){
+		var height = $(this).outerHeight();
+		var width = $(this).outerWidth();
+		$(this).css("background-image", "url('http://placekitten.com/"+width+"/"+height+"')");
+	});
+});
